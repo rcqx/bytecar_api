3983f9078004547bde25d0a1b5ec6b9cacc1bc92
fabianofrank <fabianofrank@protonmail.com>
2022-09-22 14:38:20 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do 
+      resources :users, only:
+      resources :cars, only: [:index, :show]
+      resources :reservations, only: [:new]
+    end
+  end
 end
