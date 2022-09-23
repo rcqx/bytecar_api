4da02f47b4dedc498d0746dbfd74123a2b64e45b
fabianofrank <fabianofrank@protonmail.com>
2022-09-23 15:39:39 -0300

    Match controllers with new authentication method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,15 +4,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def respond_with(resource, _opts = {})
-    if resource.persisted?
-      render json: {
-        status: { code: 200, message: 'Signed up sucessfully.' },
-        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-      }
-    else
-      render json: {
-        status: { message: "User could not sign up. #{resource.errors.full_message.to_sentence}" }
-      }, status: :unprocessable_entity
-    end
+    resource.persisted? ? register_success : register_failed
+  end
+
+  def register_success
+    render json: { message: 'Signed up.' }
+  end
+  
+  def register_failed
+    render json: { message: "Signed up failure." }
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,23 +4,18 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: {
-      status: { code: 200, message: 'Logged in sucessfully.' },
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
+    render json: { message: 'Logged.' }, status: :ok
+  end
+  
+  def respond_to_on_destroy
+    current_user ? log_out_success : log_out_failure
   end
 
-  def respond_to_on_destroy
-    if current_user
-      render json: {
-        status: 200,
-        message: 'Logged out sucessfully.'
-      }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: 'User is not logged in.'
-      }, status: :unauthorized
-    end
+  def log_out_success
+    render json: { message: "Logged out." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "Logged out failure."}, status: :unauthorized
   end
 end
