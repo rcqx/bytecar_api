ee2e7ca428996d680200bd2ddbd4463b98e9f229
fabianofrank <fabianofrank@protonmail.com>
2022-09-28 13:28:13 -0300

    Delete log_out method on the API

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,17 +5,11 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: user_params[:username])
 
     if @user
-      log_out
       token = jwt_encode(@user)
       render json: { user: @user.username, token: }, status: :ok
     else
       render json: { error: 'Incorrect user' }, status: :not_found
     end
-  end
-
-  def log_out
-    reset_session
-    @current_user = nil
   end
 
   private
